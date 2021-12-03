51c34e782b064d11214012ea7ff098b6e6eea3ba
shunmugasundarraj <shunmugasundararaja07@gmail.com>
2021-12-03 15:32:04 +0530

    initialice

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Edit <code>src/App.js</code> and save to reload.
-        </p>
+        </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,6 +15,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          <div>
+            <span>hiiiii</span>
+           
+            
+          </div>
         </a>
       </header>
     </div>
